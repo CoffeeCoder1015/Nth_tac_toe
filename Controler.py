0351c5ed23633286ff8532949f2d7b67c8e27cc3
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2020-08-02 09:28:46 +1200

    gameboard display update

--- Controler.py
+++ Controler.py
@@ -1,36 +1,40 @@
 import nth_tac_toe
 
-testGame = nth_tac_toe.Game(3)
+boardSize = 3
+
+testGame = nth_tac_toe.Game(boardSize)
 
 def X_manual():
     while 1:
         x_ip = input("x position:")
         x_ip = x_ip.split(" ")
-        rc = testGame.updateBoard("x",x_ip)
+        rc = testGame.updateBoard("x", x_ip)
         print(rc)
         if rc == None:
             testGame.display()
             break
 
+
 def O_manual():
     while 1:
         o_ip = input("o position:")
         o_ip = o_ip.split(" ")
-        rc = testGame.updateBoard("o",o_ip)
+        rc = testGame.updateBoard("o", o_ip)
         if rc == None:
             testGame.display()
             break
+
 
 sd = 0
 while 1:
     if sd == 0:
         testGame.display()
-        sd +=1
+        sd += 1
 
     X_manual()
     if testGame.curr_playing == False:
         break
-    
+
     O_manual()
     if testGame.curr_playing == False:
         break
--- nth_tac_toe.py
+++ nth_tac_toe.py
@@ -1,13 +1,16 @@
 import numpy as np
+from termcolor import colored
+
+
 class Game:
 
     curr_playing = True
 
-    def __init__ (self,boardSize):
+    def __init__(self, boardSize):
         self.boardSize = boardSize
-        self.gameBoard =  np.full((boardSize,boardSize),"-")
+        self.gameBoard = np.full((boardSize, boardSize), "-")
 
-    def updateBoard(self,player,positionVector):
+    def updateBoard(self, player, positionVector):
         if self.curr_playing == False:
             print("Game has ended")
             return 0
@@ -19,7 +22,7 @@ class Game:
         x = int(positionVector[0])-1
         y = int(positionVector[1])-1
         if x > self.boardSize or y > self.boardSize:
-            print("max size is",self.boardSize)
+            print("max size is", self.boardSize)
             return 6
 
         if self.gameBoard[x][y] != "-":
@@ -29,14 +32,14 @@ class Game:
         self.check()
 
     def check(self):
-        str_checks = None           
-        up_checks = None 
+        str_checks = None
+        up_checks = None
         crs_checks = None
-        checks = ["x","o"]    
+        checks = ["x", "o"]
         crs_c = []
-        crs_r = []   
+        crs_r = []
 
-        i=0
+        i = 0
         while 1:
 
             if i == self.boardSize:
@@ -46,61 +49,71 @@ class Game:
             crs_c.extend(self.gameBoard[i][i])
             crs_r.extend(self.gameBoard[i][self.boardSize-(i+1)])
 
-            r=0
+            r = 0
             while 1:
                 if r == self.boardSize:
                     break
                 idt = self.gameBoard[r][i]
                 rws.extend(idt)
-                r+=1
+                r += 1
 
             unique, counts = np.unique(self.gameBoard[i], return_counts=True)
-            unique,counts=list(unique),list(counts)       
+            unique, counts = list(unique), list(counts)
 
             for C in checks:
                 try:
                     _up_amm = rws.count(C)
                     _str_amm = counts[int(unique.index(C))]
-                    up_comb = (C,_up_amm)
-                    str_comb = (C,_str_amm)
+                    up_comb = (C, _up_amm)
+                    str_comb = (C, _str_amm)
                     if _up_amm == self.boardSize:
                         up_checks = up_comb
                     if _str_amm == self.boardSize:
                         str_checks = str_comb
                 except:
                     pass
-            i+=1
+            i += 1
 
         for i in checks:
             try:
                 _crs_amm1 = crs_c.count(i)
                 _crs_amm2 = crs_r.count(i)
 
-                comb_1=(i._crs_amm1)
-                comb_2=(i._crs_amm2)
+                comb_1 = (i._crs_amm1)
+                comb_2 = (i._crs_amm2)
 
                 if _crs_amm1 == self.boardSize:
                     crs_checks = comb_1
 
                 if _crs_amm2 == self.boardSize:
-                    crs_checks = comb_2                
+                    crs_checks = comb_2
             except:
                 pass
 
-        cklp = [str_checks,up_checks,crs_checks]
+        cklp = [str_checks, up_checks, crs_checks]
         for i in cklp:
             if i != None:
-
-                print(i[0],"has won--")
+                print(colored(i[0], "red"), "has won")
                 self.curr_playing = False
 
-    def display(self,mode="Parsed"):
-        if mode.lower() == "plain":
-            print(self.gameBoard)
-        if mode.lower() == "parsed":
-            xy_row = [i+1 for i in range(0,self.boardSize)]
-            print("/ "+str(xy_row).replace("[","").replace("]","").replace(",",""))
-            for i in range(0,self.boardSize):
-                itms = str(self.gameBoard[i]).replace("[","").replace("]","").replace("'","")
-                print(i+1,itms)
+    def display(self):
+        xy_row = [format(i+1, "0%s" %(len(str(self.boardSize)))) for i in range(0, self.boardSize)]
+        xyl_fmt = "/ "
+
+        if self.boardSize >= 10:
+            xyl_fmt+=" "
+
+        print(colored(
+            xyl_fmt+" ".join(xy_row), "yellow")
+        )
+        ms = ""
+        for i in range(0, self.boardSize):
+            itms = " ".join(self.gameBoard[i])
+            if self.boardSize >= 10:              
+                itms = itms.replace("-"," "+"-")
+
+            Id = colored(format(i+1, "0%s" %
+                                (len(str(self.boardSize)))), "yellow")
+            ms+=f"{Id} {itms}\n".format()
+        print(ms)
         self.check()
